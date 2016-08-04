8ebfd9d952c6308e621054703dbae6bb70d52db6
kyh5392 <kyh90495392@gmail.com>
2016-08-04 18:34:29 +0900

    추가 수정사항 완료

--- index.php
+++ index.php
@@ -26,38 +26,36 @@
         </section>
         <article class="index-notice">
             <div class="container">
-                <ul>
-                    <li>
-                        <h3><a href="#">공지사항</a></h3>
-                        <ul>
-                            <li><a href="#">NCS학사정보시스템 성적입력 안내</a></li>
-                            <li><a href="#">NCS 기반 교육과정 운영 워크숍 개최</a></li>
-                            <li><a href="#">2015년 교직원 하계워크숍 개최</a></li>
-                            <li><a href="#">2015년 2학기 적용 교수학습지침서 개발</a></li>
-                            <li><a href="#">2015년 교직원 하계워크숍 개최</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <h3>NCS 연락처 안내</h3>
-                        <p>연락주시면 친절히 상담해드리겠습니다.</p>
-                        <dl class="inline-dl">
-                            <dt>Tel</dt>
-                            <dd>063.260.9109</dd>
-                            <dt>Fax</dt>
-                            <dd>063.263.3604</dd>
-                        </dl>
-                    </li>
-                    <li>
-                        <h3><a href="#">NCS 관련뉴스</a></h3>
-                        <ul>
-                            <li><a href="#">특성화전문대학육성사업 성과보고회 개최</a></li>
-                            <li><a href="#">2015년 특성화전문대학육성사업 우수성과 사례로사례로사례로</a></li>
-                            <li><a href="#">특성화전문대학육성사업 성과보고회 개최</a></li>
-                            <li><a href="#">2015년 특성화전문대학육성사업 우수성과 사례로</a></li>
-                            <li><a href="#">특성화전문대학육성사업 성과보고회 개최</a></li>
-                        </ul>
-                    </li>
-                </ul>
+               <div class="contact">
+                    <h3>NCS 연락처 안내</h3>
+                    <p>연락주시면 친절히 상담해드리겠습니다.</p>
+                    <dl class="inline-dl">
+                        <dt>Tel</dt>
+                        <dd>063.260.9109</dd>
+                        <dt>Fax</dt>
+                        <dd>063.263.3604</dd>
+                    </dl>
+                </div>
+                <div class="notice">
+                    <h3><a href="#">공지사항</a></h3>
+                    <ul>
+                        <li><a href="#">NCS학사정보시스템 성적입력 안내</a></li>
+                        <li><a href="#">NCS 기반 교육과정 운영 워크숍 개최</a></li>
+                        <li><a href="#">2015년 교직원 하계워크숍 개최</a></li>
+                        <li><a href="#">2015년 2학기 적용 교수학습지침서 개발</a></li>
+                        <li><a href="#">2015년 교직원 하계워크숍 개최</a></li>
+                    </ul>
+                </div>
+                <div class="news">
+                    <h3><a href="#">NCS 관련뉴스</a></h3>
+                    <ul>
+                        <li><a href="#">특성화전문대학육성사업 성과보고회 개최</a></li>
+                        <li><a href="#">2015년 특성화전문대학육성사업 우수성과 사례로사례로사례로</a></li>
+                        <li><a href="#">특성화전문대학육성사업 성과보고회 개최</a></li>
+                        <li><a href="#">2015년 특성화전문대학육성사업 우수성과 사례로</a></li>
+                        <li><a href="#">특성화전문대학육성사업 성과보고회 개최</a></li>
+                    </ul>
+                </div>
             </div>
         </article>
 	</main>
